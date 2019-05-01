8a1e07de383cfb977aa1c008495e123fb52e19d9
Sarjan Narwan <sarjannarwan@Sarjans-MacBook-Pro.local>
2019-05-01 21:21:50 +0100

    Separate function for sum digits

--- id16 - power digit sum/main.cc
+++ id16 - power digit sum/main.cc
@@ -7,23 +7,28 @@
 using namespace std;
 using boost::multiprecision::cpp_int;
 
-int main(){
-    cpp_int value = 2;
-    for (int i = 0; i < 999; i++){
-        value *= 2;
-    }
-
+int sum_integer_digits(cpp_int value){
     stringstream ss;
     ss << value;
 
     string num_string = ss.str();
-    cout << num_string << endl;
 
     int sum = 0;
 
     for (int i = 0; i < num_string.length(); i++){
         sum += num_string[i] - '0';
     }
+
+    return sum;
+}
+
+int main(){
+    cpp_int value = 2;
+    for (int i = 0; i < 999; i++){
+        value *= 2;
+    }
+
+    int sum = sum_integer_digits(value);
 
     cout << sum << endl;
 }
